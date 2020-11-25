d32825a32e8bbf18a488cfe6631641f33e350a5b
Roksana Dzioba <roksanadzioba@gmail.com>
2020-11-25 16:21:12 +0100

    Updates

---
+++ precache-manifest.3de492cf204d955ac4e624e47424e1c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccdffeac59ffef8dc4ea4fbbba11099",
+    "url": "/meme-react-app/index.html"
+  },
+  {
+    "revision": "01ffd38741390b8f976f",
+    "url": "/meme-react-app/static/css/main.f6b0f7c1.chunk.css"
+  },
+  {
+    "revision": "e03d70b5bc25d36721f7",
+    "url": "/meme-react-app/static/js/2.91a5beed.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/meme-react-app/static/js/2.91a5beed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ffd38741390b8f976f",
+    "url": "/meme-react-app/static/js/main.be20a70a.chunk.js"
+  },
+  {
+    "revision": "3e71832779721e2b931c",
+    "url": "/meme-react-app/static/js/runtime-main.9e7fd7e2.js"
+  },
+  {
+    "revision": "a96d61ede1b04980baed0cb7caad5d05",
+    "url": "/meme-react-app/static/media/meme.a96d61ed.png"
+  }
+]);
